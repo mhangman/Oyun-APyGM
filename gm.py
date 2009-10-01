15c3d95187e3be57e2f0d4f53f4f7ab2e68e5f74
Ceyhun Alyesil <ceyhunalyesil@gmail.com>
2009-10-01 17:59:41 +0300

    we have admin panel huh?

--- gm.py
+++ gm.py
@@ -1,1 +1,24 @@
- 
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from mounster import *
+
+print "You must create tables before to start adding mounster type D if you want so"
+print "To add Mounster to DB write M to quit this script just write Q"
+print "To view mounsters just type W"
+edit = raw_input("Type here: ")
+data = str(edit)
+M = "M"
+Q = "Q"
+W = "W"
+D = "D"
+if data == M:
+	addMounster()
+elif data == Q:
+	sys.exit()
+elif data == W:
+	viewMounster()
+elif data == D:
+	npcDB()
+	
+
--- mounster.py
+++ mounster.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from pysqlite2 import dbapi2 as sqlite
+import sys
 
 def addMounster():
 	connection = sqlite.connect('npc.db')
 	memoryConnection = sqlite.connect(':memory:')
-	cursor = connection.cursor() 
-	
-	cursor.execute('CREATE TABLE npc (id INTEGER PRIMARY KEY, name VARCHAR(30), hp INTEGER, defance INTEGER, resist INTEGER, level INTEGER, exp INTEGER)')
+	cursor = connection.cursor()
 
 	name = raw_input("Mounster's name: ")
 	hp = raw_input("Mounster's Hitpoint: ")
@@ -19,3 +18,35 @@ def addMounster():
 
 	cursor.execute('INSERT INTO npc VALUES (null, ?, ?, ?, ?, ?, ?)', (name, hp, defance, resist, level, exp))
 	connection.commit()
+	print "mounster %s is added" % name
+	print "if you want add more just type A"
+	keep = raw_input("Type: ")
+	A = "A"
+	if keep == A:
+		return addMounster()
+	else:
+		sys.exit()
+def npcDB():
+	connection = sqlite.connect('npc.db')
+	memoryConnection = sqlite.connect(':memory:')
+	cursor = connection.cursor() 
+	
+	cursor.execute('CREATE TABLE npc (id INTEGER PRIMARY KEY, name VARCHAR(30), hp INTEGER, defance INTEGER, resist INTEGER, level INTEGER, exp INTEGER)')
+
+	connection.commit()
+	print "Tables created"
+	print "if you want add mounsters just type A"
+	keep = raw_input("Type: ")
+	A = "A"
+	if keep == A:
+		return addMounster()
+	else:
+		sys.exit()
+
+def viewMounster():
+	connection = sqlite.connect('npc.db')
+	memoryConnection = sqlite.connect(':memory:')
+	cursor = connection.cursor()
+	cursor.execute('SELECT * FROM npc')
+      
+	print cursor.fetchall()
