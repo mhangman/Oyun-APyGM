c1d5cdd6df3b00458b4efc17590117c364b222f6
Ceyhun Alyesil <ceyhunalyesil@gmail.com>
2009-10-15 15:57:58 +0300

    started market

---
+++ market.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#APyGM coding by Ceyhun Alyeşil (ceyhunalyesil@gmail.com)
+#license:gplv3
+
+from pysqlite2 import dbapi2 as sqlite
+import sys
+
+#well this file will include our market's functions. first of all at the market we should have a list of item prices
+#second we should have items and list of their cost. normally i could prefer to save this data at db. but donno we will try
+#lets first save it on a list or dictionary than we can transfer this values to db if it become nessecary.
+
+def welcomeToMarket():
+	print """Greetings your hero. This is our town's market. You can sell and buy goods here. We dont have much items,
+	you know these is hard times we have because of war. But in the future, ofcourse with your help we can make
+	better work. Now what you want?"""
+	print "To view what items you have type W"
+	print "To sell all your items type S"
+	print "To view what we have at the market just type M"
+	youwant = raw_input("What you want: ")
+	youwant = str(youwant)
+	
+	W = "W"
+	S = "S"
+	M = "M"
+
+	if youwant == W:
+		viewItems()
+	elif youwant == S:
+		sellYourItems()
+	elif youwant == M:
+		viewMarket()
+	else:
+		sys.quit()
+
+def viewItems():
+	return welcomeToMarket()
+def sellYourItems():
+	return welcomeToMarket()
+
+def viewMarket():
