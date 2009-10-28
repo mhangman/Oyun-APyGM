d5c1da3edc3ed3238c2e6a8397c0f3364383fd59
unknown <ceyhun@.(none)>
2009-10-28 13:57:51 +0200

    pyglet started

--- apygm.py
+++ apygm.py
@@ -1,1 +1,24 @@
-windows git test windows git test
+#!usr/bin/env python
+
+#ok here we started to work with pyglet. this file will be main game file and
+#will replaced with game.py
+import pyglet
+
+window = pyglet.window.Window(800*600, caption='APyGM')
+
+label = pyglet.text.Label('Hello world',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+
+@window.event
+def on_key_press(symbol, modifiers):
+    print 'A key was pressed'
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+pyglet.app.run()    
